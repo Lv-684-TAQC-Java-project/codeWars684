9007b5e3b81efe0723c26c50ea3a9a6a5ec5d10e
Liubomyr Halamaha <galamagal86@gmail.com>
2022-04-05 12:26:28 +0300

    added tests folder

---
+++ src/test/java/com/org/ita/kata/EightTest.java
@@ -0,0 +1,44 @@
+package com.org.ita.kata;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class EightTest {
+
+    @Test
+    public void testLiters() {
+    }
+
+    @Test
+    public void testGetVolumeOfCuboid() {
+    }
+
+    @Test
+    public void testMpgToKPM() {
+    }
+
+    @Test
+    public void testSquareOrSquareRoot() {
+    }
+
+    @Test
+    public void testCountPositivesSumNegatives() {
+    }
+
+    @Test
+    public void testStringToNumber() {
+    }
+
+    @Test
+    public void testAmIWilson() {
+    }
+
+    @Test
+    public void testTwoDecimalPlaces() {
+    }
+
+    @Test
+    public void testDivisibleBy() {
+    }
+}
---
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -0,0 +1,32 @@
+package com.org.ita.kata;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class FiveTest {
+
+    @Test
+    public void testArtificialRain() {
+    }
+
+    @Test
+    public void testGap() {
+    }
+
+    @Test
+    public void testZeros() {
+    }
+
+    @Test
+    public void testPerimeter() {
+    }
+
+    @Test
+    public void testSolveSum() {
+    }
+
+    @Test
+    public void testSmallest() {
+    }
+}
