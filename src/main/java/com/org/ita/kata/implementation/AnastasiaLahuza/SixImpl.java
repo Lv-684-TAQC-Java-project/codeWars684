950af284737fb2938c40f6d77092e1df9ff80489
Anastasiia <asenka88@gmail.com>
2022-04-06 12:29:13 +0300

    Added Easy Balance Checking

--- src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
@@ -10,7 +10,33 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        //Zамінюємо все крім цифр, літер, крапок та будь-чого позначаючого пробіли
+        book = book.replaceAll(
+                "[^A-Za-z0-9.\\s]", " ");
+        String[] recipe = book.split("\\n");//розділяємо строку по пробілу та пишемо в массив
+        String newBook = recipe[0].trim()+"\\r\\n";
+        double round = Math.pow(10,2);
+        double sum = 0;
+        for (int i=1; i<recipe.length; i++){
+            String[] expenseList = recipe[i].replaceAll("[\\s]{2,}", " ").split(" ");
+            //System.out.println(expenseList[2]);
+            sum+=Double.parseDouble(expenseList[2]);//знаходимо сумму витрат
+            double expense =(Double.parseDouble(recipe[0])-sum);
+            //double expense = Double.parseDouble(recipe[0])-sum;
+            //System.out.println(expense);
+            recipe[i] = recipe[i] + " Balance " + String.format("%.2f", expense);
+            newBook+=recipe[i].replaceAll("[\\s]{2,}", " ")+"\\r\\n";
+            // System.out.println(recipe[i]);
+        }
+        //Загальні витрати
+        double totalExpense = Math.round(sum*round)/round;
+        //Delta витрат
+        double averageExpense = totalExpense/(recipe.length-1);
+
+        newBook = "Original Balance: " + newBook + "Total expense  " +String.format("%.2f",  totalExpense) + "\\r\\n" +"Average expense  " + String.format("%.2f", averageExpense);
+
+        return newBook;
+
     }
 
     @Override
