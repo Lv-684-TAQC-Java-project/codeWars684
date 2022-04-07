bed719e716aff13df28d991b507e67fa8beb3a0c
Anton Shcherbei <starteran2gmail.com>
2022-04-07 22:53:14 +0300

    added newAvg

--- src/main/java/com/org/ita/kata/implementation/Antress/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Antress/SevenImpl.java
@@ -5,7 +5,19 @@ import com.org.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        // your code
+        double sum = 0;
+        long result = 0;
+
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+
+        result = (long) (Math.ceil((navg * (arr.length + 1))-sum));
+        if (result <= 0){
+            throw new IllegalArgumentException();
+        }
+        return result;
     }
 
     @Override
