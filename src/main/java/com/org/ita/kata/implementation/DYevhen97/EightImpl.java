5e940d002dd10d059cb59d7f54f4211173f46ec1
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:31:01 +0300

    new Formatting decimal places

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
@@ -40,7 +40,7 @@ public class EightImpl implements Eight {
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        return (double)Math.round(number*100)/100;
     }
 
     @Override
