78bd311b0be146ff6e4ed32498563e1cd36007ab
Anastasiia <asenka88@gmail.com>
2022-04-07 11:04:10 +0300

    Added Help the bookseller !

--- src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
@@ -35,6 +35,40 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        String book = "";
+
+        if (lstOf1stLetter==null || lstOfArt==null || lstOf1stLetter.length == 0 || lstOfArt.length == 0 ) {
+            book = "";
+        }
+        int sum =0;
+        for (int i=0; i< lstOf1stLetter.length; i++) {
+            String currentBook= lstOf1stLetter[i];
+            int count =0;
+
+            // System.out.println(currentBook);
+            for (String currentArt: lstOfArt) {
+                // System.out.println(currentArt);
+                if (currentArt.substring(0,1).equals(currentBook)) {
+                    String[] artValues = currentArt.split(" ");
+                    count += Integer.parseInt(artValues[1]);
+                    sum+=count;
+                }
+            }
+            //System.out.println(count);
+            //format the count of the book/stock
+
+            String line = "(" + currentBook + " : " + count + ")";
+            // System.out.println(stock);
+            if (i != lstOf1stLetter.length - 1) {
+                book += line + " - ";
+            } else {
+                book += line;
+            }
+        }
+        if(sum==0){
+            book="";
+        }
+        return book;
+
     }
 }
