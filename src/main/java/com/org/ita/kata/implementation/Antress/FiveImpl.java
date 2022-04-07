f7c30fadba28b284839c3e15a51952231501f5cf
Anton Shcherbei <starteran2gmail.com>
2022-04-07 23:06:44 +0300

    added gap in primes

--- src/main/java/com/org/ita/kata/implementation/Antress/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Antress/FiveImpl.java
@@ -3,6 +3,8 @@ package com.org.ita.kata.implementation.Antress;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FiveImpl implements Five {
     @Override
@@ -12,7 +14,34 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        // your code
+        long firstGap = 0;
+        long secondGap = 0;
+        long count = 0;
+        long[] result = new long[0];
+        List<Long> list = new ArrayList<>();
+
+        for (long i = m; i < n; i++) {
+            BigInteger firstInteger = BigInteger.valueOf(i);
+            count = i;
+            if (firstInteger.isProbablePrime((int) Math.log(i))) {
+                list.add(i);
+            }
+        }
+        Long[] arr = new Long[list.size()];
+        arr = list.toArray(arr);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (arr[i+1]-arr[i]==g){
+                result = new long[2];
+                result[0] = arr[i];
+                result[1] = arr[i+1];
+                break;
+            }else{
+                result = null;
+            }
+        }
+        return result;
     }
 
     @Override
