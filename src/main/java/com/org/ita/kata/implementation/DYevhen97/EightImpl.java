7ae08240e59e080327ad55660f3448ad3df9afea
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:21:05 +0300

    new Count of positives  sum of negatives

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
@@ -25,7 +25,13 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
+        if (input == null || input.length == 0) return new int[] {};
+        int count = 0, sum = 0;
+        for (int i : input) {
+            if (i > 0) count ++;
+            if (i < 0) sum += i;
+        }
+        return new int[] {count, sum};
     }
 
     @Override
