697ddc6717e7a7809dd15170ab8b8fe55531c263
Anton Shcherbei <starteran2gmail.com>
2022-04-07 22:55:15 +0300

    added seriesSum

--- src/main/java/com/org/ita/kata/implementation/Antress/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Antress/SevenImpl.java
@@ -10,7 +10,27 @@ public class SevenImpl implements Seven {
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        // Happy Coding ^_^
+        double rise = 3;
+        double sum = 0;
+        double grow = 0;
+        String result = "0.00";
+
+        if (n > 0) {
+            for (int i = 0 , j = 1; i < n; i++) {
+                if (i == 0) {
+                    sum = 1;
+                } else if (i == 1) {
+                    sum = sum + 0.25;
+                    grow = 4;
+                } else {
+                    sum += (1 / (grow + rise * j));
+                    j++;
+                }
+            }
+            result = String.format("%.2f",sum);
+        }
+        return result;
     }
 
     @Override
