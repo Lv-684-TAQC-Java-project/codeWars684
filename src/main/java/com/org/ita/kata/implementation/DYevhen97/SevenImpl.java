39acb40205fcfe35b3b678937a1e39b68af4b5ae
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:38:10 +0300

    new Looking for a benefactor

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/SevenImpl.java
@@ -5,7 +5,16 @@ import com.org.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        return 0;
+        double sum = 0;
+        for (double number : arr) {
+            sum += number;
+        }
+
+        long res = (long) Math.ceil(navg * (arr.length + 1) - sum);
+        if (res <= 0)
+            throw new IllegalArgumentException();
+        else
+            return res;
     }
 
     @Override
