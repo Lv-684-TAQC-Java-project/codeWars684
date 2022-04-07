50a354be093cd68865341ce2400beeda3ffaf9ff
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:49:27 +0300

    new Easy Balance Checking

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/SixImpl.java
@@ -10,7 +10,14 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        var lines = book.replaceAll("[^\\w\n. ]", "").split("\n");
+        var report = new StringBuilder("Original Balance: " + lines[0] + (book = "\\r\\n"));
+        double balance = Double.parseDouble(lines[0]), sum = 0;
+        for (var i = 1; i < lines.length; i++) {
+            sum += Double.parseDouble(lines[i].split("\\s+")[2]);
+            report.append(lines[i].trim().replaceAll("\\s+", " ")).append(String.format(" Balance %.2f", balance - sum)).append(book);
+        }
+        return report + String.format("Total expense  %.2f%sAverage expense  %.2f", sum, book, sum / (lines.length - 1));
     }
 
     @Override
