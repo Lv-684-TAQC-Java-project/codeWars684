5ac9b1815460ed48a68283359491d0f1d0aba17d
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:28:01 +0300

    new Wilson prime

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/EightImpl.java
@@ -35,7 +35,12 @@ public class EightImpl implements Eight {
 
     @Override
     public boolean amIWilson(double n) {
-        return false;
+        double mod = n*n;
+        double fact = 1;
+        for(int i=2;i<n;i++){
+            fact = (fact*i)%mod;
+        }
+        return fact+1 == mod;
     }
 
     @Override
