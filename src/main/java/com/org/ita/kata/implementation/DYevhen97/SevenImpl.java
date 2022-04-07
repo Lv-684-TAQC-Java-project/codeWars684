c37d3aa9293cd047cb044fae9e73a8b1eb4d7d29
DYevhen97 <yevhendamrin@gmail.com>
2022-04-07 20:40:32 +0300

    new Sum of the first nth term of Series

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/SevenImpl.java
@@ -10,7 +10,24 @@ public class SevenImpl implements Seven {
 
     @Override
     public String seriesSum(int n) {
-        return null;
+        float result = 1.0f;
+        float baseNum = 1.0f;
+        if (n <= 0) result = 0;
+        else {
+            for (int i = 1; i < n; i++) {
+                baseNum = baseNum + 3;
+                result += 1.0 / baseNum;
+            }
+        }
+        return String.format("%.2f", result);
+    }
+
+    public static String bestSolution(int n) {
+        double sum = 0.0;
+        for (int i = 0; i < n; i++)
+            sum += 1.0 / (1 + 3 * i);
+
+        return String.format("%.2f", sum);
     }
 
     @Override
