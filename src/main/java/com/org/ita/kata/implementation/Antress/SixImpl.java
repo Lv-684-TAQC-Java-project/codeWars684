186439b9fd761608d11206af5b0e394a2cafd33c
Anton Shcherbei <starteran2gmail.com>
2022-04-08 18:15:51 +0300

    added balance

--- src/main/java/com/org/ita/kata/implementation/Antress/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Antress/SixImpl.java
@@ -10,7 +10,25 @@ public class SixImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        String t = book.replaceAll("([^\\n. \\da-zA-Z])", "");
+        String[] arr = t.split("[\\n]+");
+        Double amount = Double.parseDouble(arr[0]);
+        double total = 0;
+        int count =0;
+        StringBuilder result = new StringBuilder();
+        result.append("Original Balance: " + arr[0]);
+        for (int i = 1; i < arr.length; i++) {
+            count ++;
+            String[] line = arr[i].split("[ ]+");
+            amount -= Double.parseDouble(line[2]);
+            total += Double.parseDouble(line[2]);
+            String u = String.format("\\r\\n%s %s %s Balance %.2f", line[0], line[1], line[2], amount);
+            result.append(u);
+        }
+        result.append(String.format("\\r\\nTotal expense  %.2f\\r\\nAverage expense  %.2f", total, total / count));
+        String resultStr = result.toString();
+
+        return resultStr;
     }
 
     @Override
