ae37503a5cb655638f3eb29cde45acbfe3e22e17
Anastasiia <asenka88@gmail.com>
2022-04-08 21:19:57 +0300

    Added Ranking NBA teams

--- src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/SixImpl.java
@@ -2,6 +2,9 @@ package com.org.ita.kata.implementation.AnastasiaLahuza;
 
 import com.org.ita.kata.Six;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class SixImpl implements Six {
     @Override
     public long findNb(long m) {
@@ -9,35 +12,7 @@ public class SixImpl implements Six {
     }
 
     @Override
-    public String balance(String book) {
-        //Zамінюємо все крім цифр, літер, крапок та будь-чого позначаючого пробіли
-        book = book.replaceAll(
-                "[^A-Za-z0-9.\\s]", " ");
-        String[] recipe = book.split("\\n");//розділяємо строку по пробілу та пишемо в массив
-        String newBook = recipe[0].trim()+"\\r\\n";
-        double round = Math.pow(10,2);
-        double sum = 0;
-        for (int i=1; i<recipe.length; i++){
-            String[] expenseList = recipe[i].replaceAll("[\\s]{2,}", " ").split(" ");
-            //System.out.println(expenseList[2]);
-            sum+=Double.parseDouble(expenseList[2]);//знаходимо сумму витрат
-            double expense =(Double.parseDouble(recipe[0])-sum);
-            //double expense = Double.parseDouble(recipe[0])-sum;
-            //System.out.println(expense);
-            recipe[i] = recipe[i] + " Balance " + String.format("%.2f", expense);
-            newBook+=recipe[i].replaceAll("[\\s]{2,}", " ")+"\\r\\n";
-            // System.out.println(recipe[i]);
-        }
-        //Загальні витрати
-        double totalExpense = Math.round(sum*round)/round;
-        //Delta витрат
-        double averageExpense = totalExpense/(recipe.length-1);
-
-        newBook = "Original Balance: " + newBook + "Total expense  " +String.format("%.2f",  totalExpense) + "\\r\\n" +"Average expense  " + String.format("%.2f", averageExpense);
-
-        return newBook;
-
-    }
+    public String balance(String book) {return null;}
 
     @Override
     public double f(double x) {
@@ -56,7 +31,63 @@ public class SixImpl implements Six {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+
+        if(resultSheet.isEmpty() || toFind.isEmpty()) {
+            return "";
+        }
+
+        int winCount=0;
+        int loseCount=0;
+        int drawCount=0;
+        int scoredPointsSum=0;
+        int concededPointsSum=0;
+        int rating =0;
+        String group1=null;
+        String group2=null;
+        String[] rate = resultSheet.split(",");
+        for (String s : rate) {
+            Pattern p = Pattern.compile("(" + toFind + " [0-9.]{1,7})");
+            Matcher m = p.matcher(s);
+
+            if (m.find()) {
+                group1 = m.group(1).trim();
+                System.out.println(group1);
+                group2 = s.replace(group1, "").trim();
+
+                String[] findTeam = group1.split(" ");
+                double sc1=Double.parseDouble(findTeam[findTeam.length - 1]);
+                if(sc1%1!=0){
+                    return "Error(float number):"+s;
+                }
+                int score1 = (int) sc1;
+                scoredPointsSum += score1;
+                String[] anotherTeam = group2.split(" ");
+                double sc2=Double.parseDouble(anotherTeam[anotherTeam.length - 1]);
+                if(sc2%1!=0){
+                    return "Error(float number):"+s;
+                }
+                int score2 = (int) sc2;
+                concededPointsSum += score2;
+
+                if (score1 > score2) {
+                    winCount++;
+                    rating+=3;
+                } else if (score1 < score2) {
+                    loseCount++;
+                } else{ drawCount++;
+                    rating+=1;}
+            }
+        }
+        String rez;
+        if(scoredPointsSum%1!=0 || concededPointsSum%1!=0) {
+            rez="Error(float number):the concerned string";
+        }
+        else if(scoredPointsSum==0 && concededPointsSum==0){
+            rez= toFind + ":" + "This team didn't play!";}
+        else{
+            rez=toFind+":W="+ winCount+";D="+drawCount+";L="+loseCount+";Scored="+scoredPointsSum+";Conceded="+concededPointsSum +";Points=" + rating;
+        }
+        return rez;
     }
 
     @Override
