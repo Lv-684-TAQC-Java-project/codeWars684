cd984b67b1aa6db87bc1d008846d851e4ddfe95e
Volodja Zem <v.zemela@gmail.com>
2022-04-09 00:00:16 +0300

    new mean and  variance

--- src/main/java/com/org/ita/kata/implementation/Volodja85/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Volodja85/SixImpl.java
@@ -20,12 +20,71 @@ public class SixImpl implements Six {
 
     @Override
     public double mean(String town, String strng) {
-        return 0;
+
+        try{
+            String[] search = strng.split("\n");
+            String strTown = " ";
+            double sum=0;
+            for(String searching : search){
+                if (searching.contains(town + ":")){
+                    strTown = searching;
+                }
+                if(town.equals("")) {
+                    return -1;
+
+                }
+            }
+            if (strTown.isEmpty()||strTown.equals("")){
+                return -1;}
+            strTown = strTown.replaceAll("[^0-9.,]", "");
+
+
+            String[] cityData = strTown.split(",");
+            for (int i=0; i<cityData.length; i++) {
+                sum += Double.valueOf(String.valueOf(cityData[i]));
+            }
+            return sum / cityData.length;
+        }
+        catch (NumberFormatException e){
+            e.fillInStackTrace();
+            e.getMessage();
+            e.printStackTrace();
+        }return -1;
     }
 
     @Override
     public double variance(String town, String strng) {
-        return 0;
+
+        try {
+            double meanValue = mean(town, strng);
+            String[] search = strng.split("\n");
+            String strTown = " ";
+            double sum = 0;
+            for (String searching : search) {
+                if (searching.contains(town + ":")) {
+                    strTown = searching;
+                }
+
+            }
+            if (strTown.isEmpty() || strTown.equals("")) {
+                return -1;
+            }
+            strTown = strTown.replaceAll("[^0-9.,]", "");
+
+            String[] cityData = strTown.split(",");
+            for (int i = 0; i < cityData.length; i++) {
+                double varianceDouble = Double.valueOf(String.valueOf(cityData[i]));
+                double diff = varianceDouble - meanValue;
+                sum = sum + diff * diff;
+            }
+            return sum / cityData.length;
+
+        } catch (NumberFormatException e) {
+            e.fillInStackTrace();
+            e.getMessage();
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     @Override
