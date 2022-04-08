e389c88b1b4329b9281647816fc2cd17ffad6bd1
Anastasiia <asenka88@gmail.com>
2022-04-08 18:54:43 +0300

    Added Gap in Primes

--- src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/AnastasiaLahuza/FiveImpl.java
@@ -3,6 +3,7 @@ package com.org.ita.kata.implementation.AnastasiaLahuza;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class FiveImpl implements Five {
     @Override
@@ -12,8 +13,31 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        ArrayList<Long> prime=new ArrayList<>();
+
+        for (long i = m; i < n; i++) {
+            if (isPrime(i)) {
+                prime.add(i);
+            }
+
+            for (int j=0; j<prime.size()-1; j++){
+                if (prime.get(j+1)-prime.get(j)==g){
+                    return new long[]{prime.get(j), prime.get(j+1)};
+                }
+            }
+        }
+        return null;
     }
+
+    private static boolean isPrime(long i) {
+        for (long j = 2; j < i / 2; j++) {
+            if (i % j == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
 
     @Override
     public int zeros(int n) {
