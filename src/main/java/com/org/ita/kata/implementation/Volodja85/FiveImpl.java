aff37dbe1869a15314a519f51c8e632efdf24dd8
Volodja Zem <v.zemela@gmail.com>
2022-04-10 00:32:46 +0300

    new artificialRain

--- src/main/java/com/org/ita/kata/implementation/Volodja85/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/Volodja85/FiveImpl.java
@@ -7,7 +7,40 @@ import java.math.BigInteger;
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
-        return 0;
+
+        int n=v.length;
+        int maxWateredPlains = 0;
+        int[] resultLeftFlow = new int[v.length];
+        int[] resultRightFlow = new int[v.length];
+
+        if(v.length==1){
+            return  1;
+        }
+
+        if(v.length==2){
+            return  2;
+        }
+
+        for (int i = 1; i < v.length; i++) {
+            if(v[i - 1] <= v[i]){
+                resultLeftFlow[i] = resultLeftFlow[i - 1] + 1;
+            }
+            else{
+                resultLeftFlow[i]=0;}
+        }
+
+        for (int i = n-2 ; i >= 0; i--) {
+            if(v[i + 1] <= v[i]){
+                resultRightFlow[i] = resultRightFlow[i + 1] + 1;
+            }else {resultRightFlow[i]=0;
+            }
+        }
+
+        for (int i = 0; i < v.length; i++) {
+            maxWateredPlains=Math.max(resultLeftFlow[i] +resultRightFlow[i]+1 ,maxWateredPlains);
+        }
+
+        return maxWateredPlains;
     }
 
     @Override
