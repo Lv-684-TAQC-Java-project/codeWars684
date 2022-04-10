34453273926fb184e69bfc6f8aef49b2ad1ac054
OlesNikon <oles.nikon@gmail.com>
2022-04-10 20:33:50 +0300

    add Find numbers which are divisible by given number

--- src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
@@ -2,6 +2,8 @@ package com.org.ita.kata.implementation.OlesNikon;
 
 import com.org.ita.kata.Eight;
 
+import java.util.Arrays;
+
 public class EightImpl implements Eight {
     @Override
     public int liters(double time) {
@@ -45,6 +47,8 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        return Arrays.stream(numbers)
+                .filter(i -> (i % divider) == 0)
+                .toArray();
     }
 }
