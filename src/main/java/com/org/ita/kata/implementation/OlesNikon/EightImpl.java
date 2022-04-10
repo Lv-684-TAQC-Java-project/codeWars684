8d94bb0656838906e76d36ade63f5e4beddea38f
OlesNikon <oles.nikon@gmail.com>
2022-04-10 20:22:59 +0300

    add To square(root) or not to square(root)

--- src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
@@ -20,7 +20,12 @@ public class EightImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        for (int i = 0; i < array.length; i++) {
+            if (Math.sqrt(array[i]) % 1 == 0) {
+                array[i] = (int) Math.sqrt(array[i]);
+            } else {array[i] = array [i] * array [i];}
+        }
+        return array;
     }
 
     @Override
