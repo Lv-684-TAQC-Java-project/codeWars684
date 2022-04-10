c8679badba2d75bb09364c28d2329aff082ee05c
OlesNikon <oles.nikon@gmail.com>
2022-04-10 20:27:15 +0300

    add Convert a String to a Number!

--- src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OlesNikon/EightImpl.java
@@ -30,7 +30,8 @@ public class EightImpl implements Eight {
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+        int numer = Integer.parseInt(str);
+        return numer;
     }
 
     @Override
