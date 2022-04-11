6305e4d1f4703cd58c9d90c637ddc375dac50232
DGalak <dmytrohalak07@gmail.com>
2022-04-11 23:12:42 +0300

    Implement mean,variance

--- src/main/java/com/org/ita/kata/implementation/DGalak/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DGalak/SixImpl.java
@@ -1,5 +1,10 @@
 package com.org.ita.kata.implementation.DGalak;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.org.ita.kata.Six;
 
 public class SixImpl implements Six {
@@ -28,14 +33,56 @@ public class SixImpl implements Six {
         return 0;
     }
 
+    public static List<Double> getValues(String city, String data) {
+        System.out.println("counn data  " + data);
+        System.out.println("City   " + city);
+        List<Double> arrayData = new ArrayList<Double>();
+        String[] dataByCity = data.split("\\n");
+        for (int i = 0; i < dataByCity.length; i++) {
+            if (dataByCity[i].split(":")[0].equals(city)) {
+                String regex = "([0-9]+[.][0-9]+)";
+                Pattern pattern = Pattern.compile(regex);
+                Matcher matcher = pattern.matcher(dataByCity[i]);
+                while (matcher.find()) {
+                    arrayData.add(Double.valueOf(matcher.group()));
+                }
+            }
+        }
+        if (arrayData.isEmpty()) {
+            arrayData.add(-1.0);
+        }
+        return arrayData;
+    }
     @Override
     public double mean(String town, String strng) {
-        return 0;
+        List<Double> arrayRainFall = new ArrayList<Double>();
+        arrayRainFall = getValues(town, strng);
+        double counter = 0;
+        for (int i = 0; i < arrayRainFall.size(); i++) {
+            counter = counter + arrayRainFall.get(i);
+        }
+        if (arrayRainFall.get(0) == -1.0) {
+            return -1.0;
+        }
+        return (counter / arrayRainFall.size());
     }
-
     @Override
     public double variance(String town, String strng) {
-        return 0;
+        List<Double> arrayRainFallVariance = new ArrayList<Double>();
+        List<Double> arrayResultVariance = new ArrayList<Double>();
+        arrayRainFallVariance = getValues(town, strng);
+        double meanValue = mean(town, strng);
+        for (int i = 0; i < arrayRainFallVariance.size(); i++) {
+            arrayResultVariance.add(Math.pow((arrayRainFallVariance.get(i) - meanValue), 2));
+        }
+        double counter = 0;
+        for (int i = 0; i < arrayResultVariance.size(); i++) {
+            counter = counter + arrayResultVariance.get(i);
+        }
+        if (meanValue == -1.0) {
+            return -1.0;
+        }
+        return (counter / arrayResultVariance.size());
     }
 
     @Override
