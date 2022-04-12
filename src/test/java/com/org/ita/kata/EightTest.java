9cc4e64c3d3e399e47495fd0c1c8fe799e926eb9
okras2 <102805692+okras2@users.noreply.github.com>
2022-04-12 15:05:44 +0300

    Update EightTest.java

--- src/test/java/com/org/ita/kata/EightTest.java
+++ src/test/java/com/org/ita/kata/EightTest.java
@@ -9,9 +9,11 @@ public class EightTest {
     }
 
     @Test
-    public void testGetVolumeOfCuboid() {
-    }
-
+   public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    double volume =0;
+    volume = length*width*height;
+    return volume;
+  }
     @Test
     public void testMpgToKPM() {
     }
