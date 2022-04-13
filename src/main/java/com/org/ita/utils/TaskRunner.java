a6045804ac93e6b636a16589ee6b4b232752af22
maka155 <martunykmarik@gmail.com>
2022-04-14 00:04:05 +0300

    Some changes

--- src/main/java/com/org/ita/utils/TaskRunner.java
+++ src/main/java/com/org/ita/utils/TaskRunner.java
@@ -35,6 +35,13 @@ public class TaskRunner {
                 six = new com.org.ita.kata.implementation.Antress.SixImpl();
                 five = new com.org.ita.kata.implementation.Antress.FiveImpl();
                 break;
+
+            case MAKA:
+                eight = new com.org.ita.kata.implementation.maka155.EightImpl();
+                seven = new com.org.ita.kata.implementation.maka155.SevenImpl();
+                six = new com.org.ita.kata.implementation.maka155.SixImpl();
+                five = new com.org.ita.kata.implementation.maka155.FiveImpl();
+                break;
         }
     }
 
@@ -49,18 +56,11 @@ public class TaskRunner {
         System.out.println("Result: " + eight.getVolumeOfCuboid(length, width, height));
     }
 
-    public  void task_6_1(){
-        System.out.println(Tasks.TASK_6_1.getTaskName());
-        System.out.println("Enter integer value");
-        Long value = scanner.readLong();
-        System.out.println("Result: " + six.findNb(value));
-    }
-
-    public  void task_6_2(){
-        System.out.println(Tasks.TASK_6_2.getTaskName());
-        System.out.println("Enter text");
-        String text = scanner.readString();
-        System.out.println("Result: " + six.balance(text));
+    public void task_8_6() {
+        System.out.println(Tasks.TASK_8_6.getTaskName());
+        System.out.println("Enter number:");
+        String str = scanner.readString();
+        System.out.println("Result: " + eight.stringToNumber(str));
     }
 
     public void task_8_7() {
@@ -70,10 +70,26 @@ public class TaskRunner {
         System.out.println("Result: " + eight.amIWilson(n));
     }
 
-    public void task_8_6() {
-        System.out.println(Tasks.TASK_8_6.getTaskName());
-        System.out.println("Enter number:");
-        String str = scanner.readString();
-        System.out.println("Result: " + eight.stringToNumber(str));
+    public void task_8_9() {
+        System.out.println(Tasks.TASK_8_9.getTaskName());
+        System.out.println("Enter numbers");
+        int[] numbers = scanner.readIntArray();
+        System.out.println("Enter divider");
+        int divider = scanner.readInt();
+        System.out.println("Result: " + eight.divisibleBy(numbers, divider));
+    }
+
+    public void task_6_1() {
+        System.out.println(Tasks.TASK_6_1.getTaskName());
+        System.out.println("Enter integer value");
+        Long value = scanner.readLong();
+        System.out.println("Result: " + six.findNb(value));
+    }
+
+    public void task_6_2() {
+        System.out.println(Tasks.TASK_6_2.getTaskName());
+        System.out.println("Enter text");
+        String text = scanner.readString();
+        System.out.println("Result: " + six.balance(text));
     }
 }
