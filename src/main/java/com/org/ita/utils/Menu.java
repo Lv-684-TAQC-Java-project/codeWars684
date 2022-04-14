9c6a647e1052e661d07be65f0dadc0c55fbd6c13
Anastasiia <asenka88@gmail.com>
2022-04-14 12:15:13 +0300

    Renewed conditions for tasks choise

--- src/main/java/com/org/ita/utils/Menu.java
+++ src/main/java/com/org/ita/utils/Menu.java
@@ -1,5 +1,6 @@
 package com.org.ita.utils;
 
+
 public class Menu {
     private final ConsoleScanner scanner = new ConsoleScanner();
     private final TaskRunner taskRunner = new TaskRunner();
@@ -7,6 +8,8 @@ public class Menu {
     public boolean run() {
         int choice;
         do {
+            showMenu();
+            choice = scanner.readInt();
             while (true) {
                 try {
                     showMenu();
@@ -32,12 +35,14 @@ public class Menu {
 
                     System.out.println();
                     int user_id = scanner.readInt();
+                    Users users = Users.getById(user_id);
+                    taskRunner.setUser(users);
                     if (user_id < 1 || user_id >8) {
-                    System.out.println("No such user, try again\n");
-                    break;
-                }
-                Users users = Users.getById(user_id);
-                taskRunner.setUser(users);
+                        System.out.println("No such user, try again\n");
+                        break;
+                    }
+                    Users users = Users.getById(user_id);
+                    taskRunner.setUser(users);
 
                 case 2:
                     System.out.println("List of tasks:");
@@ -59,11 +64,4 @@ public class Menu {
         } while (choice != 0);
         System.out.println("The program is finished!");
         return true;
-    }
-
-    public void showMenu() {
-        System.out.println("1 - set user");
-        System.out.println("2 - run task");
-        System.out.println("0 - Exit.");
-    }
 }
