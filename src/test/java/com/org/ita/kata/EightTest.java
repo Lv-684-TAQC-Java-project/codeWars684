e2267217e30b3a9576fcc61787e033269b1a0b39
Volodja Zem <v.zemela@gmail.com>
2022-04-16 00:38:00 +0300

    test to testDivisibleBy

--- src/test/java/com/org/ita/kata/EightTest.java
+++ src/test/java/com/org/ita/kata/EightTest.java
@@ -52,7 +52,25 @@ public class EightTest extends DataProviderUserImpl {
     public void testTwoDecimalPlaces() {
     }
 
-    @Test
-    public void testDivisibleBy() {
+    @DataProvider(name = "VolumeOfDivisibleByDP")
+    public Object[][] VolumeOfDivisibleByCombineDP() {
+        int[]a={1,2,3,4,5,6};
+        int[]b={1,2,3,4,5,6};
+        int[]c={0,1,2,3,4,5,6};
+        int[]expected={2,4,6};
+        int[]expected2={3,6};
+        int[]expected3={0,4};
+        Object[][] data = new Object[][]{
+                {a,2,expected},
+                {b,3,expected2},
+                {c,4,expected3}
+        };
+        return combine(implementationsEightKataDataProvider(), data);
+    }
+
+    @Test(dataProvider = "VolumeOfDivisibleByDP")
+    public void testDivisibleBy(Eight impl, int[] numbers, int divider, int[] expected) {
+        Assert.assertEquals(impl.divisibleBy( numbers, divider),expected);
+
     }
 }
