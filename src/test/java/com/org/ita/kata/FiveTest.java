387a573ff61ed0c8cdc01e8cbf58990119bcb83a
DGalak <dmytrohalak07@gmail.com>
2022-04-18 13:59:52 +0300

    Implement testArtificialRain

--- src/test/java/com/org/ita/kata/FiveTest.java
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -1,11 +1,27 @@
 package com.org.ita.kata;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class FiveTest {
+public class FiveTest extends DataProviderUserImpl {
 
-    @Test
-    public void testArtificialRain() {
+    @DataProvider(name = "artificialRainCombineDP")
+    public Object[][] artificialRainCombineDP() {
+                int[] a = {2};
+                int [] b = {1,2,1,2,1};
+                int [] c = {4,2,3,3,2};
+        Object[][] data = new Object[][]{
+                {a, 1,},
+                {b, 3},
+                {c, 4}
+        };
+        return combine(implementationsFiveKataDataProvider(), data);
+    }
+    @Test(dataProvider = "artificialRainCombineDP")
+    public void testArtificialRain(Five impl, int[] array, int expected) {
+        int actual = impl.artificialRain(array);
+        Assert.assertEquals(actual,expected);
     }
 
     @Test
