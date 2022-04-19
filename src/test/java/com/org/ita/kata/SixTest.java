0b349a880103442e8731f26143184f52573794c8
Anastasiia <asenka88@gmail.com>
2022-04-19 15:05:58 +0300

    Added Test Build a pile of Cubes

--- src/test/java/com/org/ita/kata/SixTest.java
+++ src/test/java/com/org/ita/kata/SixTest.java
@@ -1,11 +1,30 @@
 package com.org.ita.kata;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class SixTest {
 
-    @Test
-    public void testFindNb() {
+    @DataProvider(name = "findNb")
+    public Object[][] findNb() {
+        Object[][] data = new Object[][]{
+                {4183059834009L, 2022},
+                {24723578342962L, -1},
+                {135440716410000L, 4824},
+                {40539911473216L, 3568},
+                {108806345136785L, -1},
+                {41364076483082L,-1},
+                {9541025211025L, 2485},
+                {16,-1}
+        };
+        return combine(implementationsSixKataDataProvider(), data);
+    }
+
+    @Test(dataProvider ="findNb")
+    public void testFindNb(Six impl, long input, long expected) {
+        long actual = impl.findNb(input);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
