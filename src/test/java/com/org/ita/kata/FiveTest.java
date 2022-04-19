b1df67e6c26bb0f154a31d5dfd6bba0f0d717628
Anastasiia <asenka88@gmail.com>
2022-04-19 15:19:52 +0300

    Added test for Number of trailing zeros of N!

--- src/test/java/com/org/ita/kata/FiveTest.java
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -1,6 +1,10 @@
 package com.org.ita.kata;
 
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.math.BigInteger;
 
 public class FiveTest {
 
@@ -12,8 +16,40 @@ public class FiveTest {
     public void testGap() {
     }
 
-    @Test
-    public void testZeros() {
+    @DataProvider(name = "Zeros")
+    public Object[][] Zeros() {
+        int n = 1+(int)( Math.random()*100);
+        BigInteger result = BigInteger.ONE;
+        for (int i = 1; i <= n; i++)
+        {
+            result = result.multiply(new BigInteger(i + ""));
+        }
+
+        String str = String.valueOf(result);
+        int count = 0;
+        char[] chars = str.toCharArray();
+
+        for (int i = chars.length - 1; i >= 0; i--)
+        {
+            if (chars[i] != '0')
+                break;
+            count++;
+        }
+        System.out.println(result + ":"+count);
+
+        Object [][] data = new Object[][]{
+                {0, 0},
+                {6, 1},
+                {14, 2},
+                {n, count}
+        };
+        return combine(implementationsFiveKataDataProvider(), data);
+    }
+
+    @Test(dataProvider = "Zeros")
+    public void testZeros(Five impl, int n, int expected) {
+        double actual = impl.zeros(n);
+        Assert.assertEquals(actual, expected);
     }
 
     @Test
