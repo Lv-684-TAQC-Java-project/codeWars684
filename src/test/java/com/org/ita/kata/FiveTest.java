59e0012d29bccd92f95da879d9ccb87d530588ca
Volodja Zem <v.zemela@gmail.com>
2022-04-23 18:07:43 +0300

    add try catch to method

--- src/test/java/com/org/ita/kata/FiveTest.java
+++ src/test/java/com/org/ita/kata/FiveTest.java
@@ -79,7 +79,7 @@ public class FiveTest extends DataProviderUserImpl {
     public Object[][] SmallestCombine() {
         Object[][] data = new Object[][]{
                 {261235, new long[]{126235, 2, 0}},
-                {209917, new long[]{29917, 1, 0}},
+                {209917, new long[]{29917, 0, 1}},
                 {285365, new long[]{238565, 3, 1}},
                 {269045, new long[]{26945, 3, 0}},
                 {296837, new long[]{239687, 4, 1}}
