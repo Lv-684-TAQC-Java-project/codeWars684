d69941307f423acb9caedbfd529ee3afe9544bed
DYevhen97 <yevhendamrin@gmail.com>
2022-05-02 23:40:04 +0300

    Task #16 Ranking NBA Teams

--- src/main/java/com/org/ita/kata/implementation/DYevhen97/SixImpl.java
+++ src/main/java/com/org/ita/kata/implementation/DYevhen97/SixImpl.java
@@ -51,6 +51,19 @@ public class SixImpl implements Six {
 
     @Override
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
-        return null;
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) return "";
+
+        int sum = 0;
+        String res = "";
+
+        for (String i : lstOf1stLetter) {
+            sum = 0;
+            for (String j : lstOfArt) {
+                sum += j.substring(0,1).equals(i) ? Integer.parseInt(j.replaceAll("[^0-9]","")) : 0;
+            }
+            res += " - (" + i + " : " + sum + ")";
+        }
+
+        return res.substring(3);
     }
-}
+    }
