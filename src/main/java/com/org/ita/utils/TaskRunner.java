0f721548b2a99a8de09581ae961c605fa1234a92
Volodja Zem <v.zemela@gmail.com>
2022-05-10 00:38:52 +0300

    new task 5.6 runner

--- src/main/java/com/org/ita/utils/TaskRunner.java
+++ src/main/java/com/org/ita/utils/TaskRunner.java
@@ -78,6 +78,7 @@ public class TaskRunner {
         Double height = scanner.readDouble();
         System.out.println("Result: " + eight.getVolumeOfCuboid(length, width, height));
     }
+
     public void task_8_3() {
         System.out.println(Tasks.TASK_8_3.getTaskName());
         System.out.println("Miles per gallon:");
@@ -195,6 +196,7 @@ public class TaskRunner {
         System.out.println("Result: " + six.stockSummary(lstOfArt, lstOf1stLetter));
 
     }
+
     public void task_5_1() {
         System.out.println(Tasks.TASK_5_1.getTaskName());
         System.out.println("Enter numbers:");
@@ -202,6 +204,7 @@ public class TaskRunner {
         System.out.println("Result: " + five.artificialRain(numbers));
 
     }
+
     public void task_5_2() {
         System.out.println(Tasks.TASK_5_2.getTaskName());
         System.out.println("Enter gap:");
@@ -213,6 +216,7 @@ public class TaskRunner {
         System.out.println("Result: " + eight.getVolumeOfCuboid(g, m, n));
 
     }
+
     public void task_5_3() {
         System.out.println(Tasks.TASK_5_3.getTaskName());
         System.out.println("Enter number:");
@@ -239,10 +243,10 @@ public class TaskRunner {
         switch (scanner) {
             case 2:
                 task_8_2();
-                    break;
-                case 3:
-                    task_8_3();
-                   break;
+                break;
+            case 3:
+                task_8_3();
+                break;
                 case 4:
                     task_8_4();
                     break;
@@ -255,56 +259,56 @@ public class TaskRunner {
                 task_8_7();
                 break;
             case 8:
-                   task_8_8();
-                   break;
-               case 9:
-                    task_8_9();
-                    break;
-                case 10:
-                    task_7_1();
-                    break;
-                case 11:
-                    task_7_2();
-                    break;
+                task_8_8();
+                break;
+            case 9:
+                task_8_9();
+                break;
+            case 10:
+                task_7_1();
+                break;
+            case 11:
+                task_7_2();
+                break;
 //                case 12:
 //                    task_7_3();
 //                    break;
-                      case 13:
-                          task_6_1();
-                          break;
-                       case 14:
-                           task_6_2();
+            case 13:
+                task_6_1();
                 break;
-                case 15:
-                    task_6_3();
-                    break;
-                case 16:
-                   task_6_4();
-                    break;
-                case 17:
-                    task_6_5();
-                    break;
-                case 18:
-                    task_6_6();
-                    break;
-                case 19:
-                    task_5_1();
-                    break;
-                case 20:
-                    task_5_2();
-                    break;
-                case 21:
-                    task_5_3();
-                    break;
-                case 22:
-                    task_5_4();
-                    break;
+            case 14:
+                task_6_2();
+                break;
+            case 15:
+                task_6_3();
+                break;
+            case 16:
+                task_6_4();
+                break;
+            case 17:
+                task_6_5();
+                break;
+            case 18:
+                task_6_6();
+                break;
+            case 19:
+                task_5_1();
+                break;
+            case 20:
+                task_5_2();
+                break;
+            case 21:
+                task_5_3();
+                break;
+            case 22:
+                task_5_4();
+                break;
 //                case 23:
 //                    task_5_5();
 //                    break;
-//                case 24:
-//                    task_5_6();
-//                    break;
+            case 24:
+                task_5_6();
+                break;
             default:
                 System.exit(3);
         }
